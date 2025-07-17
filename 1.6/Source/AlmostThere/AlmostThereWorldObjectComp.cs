f312227c0ebc20b67df3818cff6a06423822b95d
Soh Thiam Hing <daffodilistic@gmail.com>
2025-07-17 15:51:06 +0800

    [fix] Update for 1.6

---
+++ 1.6/Source/AlmostThere/AlmostThereWorldObjectComp.cs
@@ -0,0 +1,90 @@
+﻿using RimWorld;
+using RimWorld.Planet;
+using System.Collections.Generic;
+using UnityEngine;
+using Verse;
+
+namespace AlmostThere
+{
+    public class AlmostThereWorldObjectComp : WorldObjectComp
+    {
+        public bool cacheDirty = true;
+        public bool cachedResult = false;
+        public bool almostThere = true;
+        public bool fullyIgnoreRest = false;
+        public bool forceRest = false;
+        public override void PostExposeData()
+        {
+            base.PostExposeData();
+            Scribe_Values.Look(ref almostThere, "almostThere", true);
+            Scribe_Values.Look(ref fullyIgnoreRest, "fullyIgnoreRest", false);
+            Scribe_Values.Look(ref forceRest, "forceRest", false);
+        }
+        public override IEnumerable<Gizmo> GetGizmos()
+        {
+            if (parent.Faction == Faction.OfPlayer)
+            {
+                yield return new Command_Toggle
+                {
+                    isActive = () => almostThere,
+                    defaultLabel = "AT_Command_AlmostThere_Label".Translate(),
+                    defaultDesc = "AT_Command_AlmostThere_Description".Translate(AlmostThereSettings.AlmostThereHours),
+                    icon = ContentFinder<Texture2D>.Get("UI/" + "AlmostThere", true),
+                    toggleAction = delegate
+                    {
+                        cacheDirty = true;
+                        almostThere = !almostThere;
+                        if (almostThere)
+                        {
+                            forceRest = false;
+                            fullyIgnoreRest = false;
+                        }
+                    }
+                };
+                yield return new Command_Toggle
+                {
+                    isActive = () => fullyIgnoreRest,
+                    defaultLabel = "AT_Command_DontRest_Label".Translate(),
+                    defaultDesc = "AT_Command_DontRest_Description".Translate(),
+                    icon = ContentFinder<Texture2D>.Get("UI/" + "DontRest", true),
+                    toggleAction = delegate
+                    {
+                        cacheDirty = true;
+                        fullyIgnoreRest = !fullyIgnoreRest;
+                        if (fullyIgnoreRest)
+                        {
+                            forceRest = false;
+                            almostThere = false;
+                        }
+                    }
+                };
+                yield return new Command_Toggle
+                {
+                    isActive = () => forceRest,
+                    defaultLabel = "AT_Command_ForceRest_Title".Translate(),
+                    defaultDesc = "AT_Command_ForceRest_Description".Translate(),
+                    icon = ContentFinder<Texture2D>.Get("UI/" + "ForceRest", true),
+                    toggleAction = delegate
+                    {
+                        cacheDirty = true;
+                        forceRest = !forceRest;
+                        if (forceRest)
+                        {
+                            almostThere = false;
+                            fullyIgnoreRest = false;
+                        }
+                    }
+                };
+            }
+            yield break;
+        }
+    }
+
+    public class WorldObjectCompProperties_AlmostThere : WorldObjectCompProperties
+    {
+        public WorldObjectCompProperties_AlmostThere()
+        {
+            compClass = typeof(AlmostThereWorldObjectComp);
+        }
+    }
+}
---
+++ 1.6/Source/AlmostThere/HarmonyPatches.cs
@@ -0,0 +1,56 @@
+﻿using HarmonyLib;
+using RimWorld;
+using RimWorld.Planet;
+using Verse;
+
+namespace AlmostThere
+{
+    [StaticConstructorOnStartup]
+    class HarmonyPatches
+    {
+        static HarmonyPatches()
+        {
+
+            var harmony = new Harmony("cat2002.almostthere");
+            harmony.PatchAll();
+        }
+    }
+    [HarmonyPatch(typeof(Caravan), "get_NightResting")]
+    class Patch_Caravan_get_NightResting
+    {
+        static void Postfix(Caravan __instance, ref bool __result)
+        {
+            //var playerCaravans = Current.Game.World.worldObjects.Caravans;
+
+            AlmostThereWorldObjectComp comp = __instance.GetComponent<AlmostThereWorldObjectComp>();
+            if (comp != null)
+            {
+                if (!__instance.IsHashIntervalTick(60) && !comp.cacheDirty)
+                {
+                    __result = comp.cachedResult;
+                    return;
+                }
+                else if (comp.forceRest)
+                {
+                    __result = true;
+                }
+                else if (comp.fullyIgnoreRest)
+                {
+                    __result = false;
+                }
+                else if (comp.almostThere)
+                {
+                    var estimatedTicks = (float)CaravanArrivalTimeEstimator.EstimatedTicksToArrive(__instance, allowCaching: true);
+                    var restTicksLeft = CaravanNightRestUtility.LeftRestTicksAt(__instance.Tile, Find.TickManager.TicksAbs);
+                    estimatedTicks -= restTicksLeft;
+                    if (estimatedTicks / GenDate.TicksPerHour < AlmostThereSettings.AlmostThereHours)
+                    {
+                        __result = false;
+                    }
+                }
+                comp.cacheDirty = false;
+                comp.cachedResult = __result;
+            }
+        }
+    }
+}
